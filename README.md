e48547a2143e0cf4a4e6ee214387f300f9bf333b
joeylin <joe.y.lin.2012@gmail.com>
2013-11-12 13:37:21 +0800

    initial

---
+++ README.md
@@ -0,0 +1,151 @@
+# jQuery dropdown
+
+The powerful jQuery plugin that creates a custom dropdown. <a href="http://amazingsurge.github.io/jquery-dropdown/">Project page and demos</a><br />
+Download: <a href="https://github.com/amazingSurge/jquery-dropdown/archive/master.zip">jquery-dropdown-master.zip</a>
+
+***
+
+## Features
+
+* **Extremely fast**
+* **Option group support**
+* **Javascript only 4KB compressed**
+* **Lightweight size** — 1 kb gzipped
+
+## Dependencies
+* <a href="http://jquery.com/" target="_blank">jQuery 1.83+</a>
+
+## Usage
+
+Import this libraries:
+* jQuery
+* jquery-dropdown.min.js
+
+And CSS:
+* jquery-dropdown.css - desirable if you have not yet connected one
+
+
+Create base html element:
+```html
+<select class="custom-select">
+    <option value="a">beijing</option>
+    <option value="b">fujian</option>
+    <option value="c">zhejiang</option>
+    <option value="d">tianjin</option>
+    <option value="e">shanghai</option>
+</select>
+```
+
+Initialize dropdown:
+```javascript
+$('.custom-dropdown').dropdown({skin: 'simple'});
+```
+
+Or initialize dropdown with custom settings:
+```javascript
+$(".custom-dropdown").dropdown({
+        namespace: 'dropdown',
+        skin: 'simple',
+        trigger: 'click', // 'hover' or 'click'
+        value: 'a',
+        offset: [0, 0],
+        status: {
+            a: 'beijing',
+            b: 'fujian',
+            c: 'zhejiang'
+        },
+        onChange: function() {}
+});
+```
+
+
+
+## Settings
+
+```javascript
+    //Optional property,set a namspace for css class, for example, we have <code>.select_active
+    //</code> class for active effect, if namespace set to 'as-select', then it will be <code>.
+    //as-select_active</code>
+    namespace: '.select',
+
+    //Optional property, set transition effect, it works after you load specified skin file
+    skin: 'simple',
+
+    //Optional property, the way to active select, optioal 'hover
+    trigger: 'click',
+
+    //Optional property, set the value of bar that element have no option when select initilized
+    value: 'a',
+
+    //Optional property, set the offset between bar and comment
+    offset: [0,0]
+
+    //Optional property, set the status of elements,for example 
+    //<code>a:beijing</code> means the element's value is 'a',
+    //the text is 'beijing'
+    status: {
+         a: 'beijing',
+         b: 'fujian',
+         c: 'zhejiang'
+    },
+
+    //callback when element is seted refresh
+    Onchange: function(){}
+```
+
+## Public methods
+
+jquery select has different methods , we can use it as below :
+```javascript
+// show comment
+$(".custom-select").select("show");
+
+// hide comment
+$(".custom-select").select("hide");
+
+// set element's status
+$(".custom-select").select("set");
+
+// get option's value
+$(".custom-select").select("get");
+
+// bar enable be actived
+$(".custom-select").select("enable");
+
+// bar can't be actived 
+$(".custom-select").select("disable");
+
+// remove all event
+$(".custom-select").select("destroy");
+```
+
+## Event / Callback
+
+* <code>change</code>: trigger when select chage
+
+how to use event:
+```javascript
+$(document).on('change', function(event,instance) {
+    // instance means current select instance 
+    // some stuff
+});
+```
+
+## Browser support
+jquery-select is verified to work in Internet Explorer 7+, Firefox 2+, Opera 9+, Google Chrome and Safari browsers. Should also work in many others.
+
+Mobile browsers (like Opera mini, Chrome mobile, Safari mobile, Android browser and others) is coming soon.
+
+## Changes
+
+| Version | Notes                                                            |
+|---------|------------------------------------------------------------------|
+|     ... | ...                                                              |
+
+## Author
+[amazingSurge](http://amazingSurge.com)
+
+## License
+jQuery-select plugin is released under the <a href="https://github.com/amazingSurge/jquery-select/blob/master/LICENCE.GPL" target="_blank">GPL licence</a>.
+
+
---
+++ src/jquery-dropdown.js
@@ -0,0 +1,204 @@
+/*
+ * jquery-dropdown
+ * https://github.com/amazingSurge/jquery-dropdown
+ *
+ * Copyright (c) 2013 amazingSurge
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+    var Dropdown = $.dropdown = function(element, options) {
+        this.element = element;
+        this.$element = $(element);
+        this.$parent = this.$element.parent();
+
+        // options
+        var meta_data = [];
+        $.each(this.$element.data(), function(k, v) {
+            var re = new RegExp("^dropdown", "i");
+            if (re.test(k)) {
+                meta_data[k.toLowerCase().replace(re, '')] = v;
+            }
+        });
+        this.options = $.extend(true, {}, Dropdown.defaults, options, meta_data);
+        this.namespace = this.options.namespace;
+        this.classes = {
+            skin: this.namespace + '_' + this.options.skin,
+            show: this.namespace + '_show',
+            trigger: this.namespace + '-trigger',
+            panel: this.namespace + '-panel'
+        };
+
+        // skin
+        if (this.options.skin !== null) {
+            this.$element.addClass(this.classes.skin);
+        }
+
+        // content
+        this.$panel = this._parse(this.options.panel);
+
+        //state
+        this.isShow = false;
+        this.enabled = true;
+
+        //init
+        this.init();
+    };
+
+    Dropdown.prototype = {
+        constructor: Dropdown,
+        init: function() {
+            var self = this;
+            this.$parent.css({position:'relative'});
+            this.$element.addClass(this.namespace).addClass(this.classes.trigger);
+            this.$panel.addClass(this.classes.panel);
+            this.$element.on('click.dropdown', function() {
+                self.toggle.call(self);
+                return false;
+            });
+            if (typeof this.options.onInit === 'function') {
+                this.options.onInit(this);
+            }
+            this.$element.trigger('dropdown::init', this);
+        },
+        show: function() {
+            if (this.enabled === false) {
+                return;
+            }
+            this._bindActionEvent();
+            this.isShow = true;
+            this.$element.addClass(this.classes.show);
+            this.$panel.addClass(this.classes.show);
+            
+            this._position();
+
+            if (typeof this.options.onShow === 'function') {
+                this.options.onShow(this);
+            }
+            this.$element.trigger('dropdown::show', this);
+        },
+        hide: function() {
+            this.isShow = false;
+            this.$element.removeClass(this.classes.show);
+            this.$panel.removeClass(this.classes.show);
+            this._unbindActionEvent();
+            if (typeof this.options.onHide === 'function') {
+                this.options.onHide(this);
+            }
+            this.$element.trigger('dropdown::hide', this);
+        },
+        toggle: function() {
+            if (this.isShow) {
+                this.hide();
+            } else {
+                this.show();
+            } 
+        },
+        _bindActionEvent: function() {
+            var self = this;
+            if (this.options.clickoutHide) {
+                // this is a bit of a hack until we have a better way to close the panel
+                self.$panel.on('click.dropdown', 'li', function() {
+                    if (typeof self.options.onChange === 'function') {
+                        self.options.onChange($(this));
+                    }
+                    self.$element.trigger('dropdown::onChange', $(this));
+                    self.hide();
+                    return false;
+                });
+                $(document).on('click.dropdown', function() {
+                    self.hide();
+                    return false;
+                });
+            }
+            $(window).on('resize.dropdown', function() {
+                self._position();
+                return false;
+            });
+        },
+        _unbindActionEvent: function() {
+            this.$panel.off('click.dropdown');
+            $(document).off('click.dropdown');
+            $(window).off('resize.dropdown');
+        },
+        _position: function() {
+            var offset = this.$element.offset(),
+                height = this.$element.outerHeight(),
+                width = this.$element.outerWidth(),
+                panelWidth = this.$panel.outerWidth(true),
+                panelHeight = this.$panel.outerHeight(true),
+                top, left;
+
+            if (panelHeight + height + offset.top > $(window).height() + $(window).scrollTop()) {
+                top =  - panelHeight;
+            } else {
+                top = height;
+            }
+            if (panelWidth + offset.left > $(window).width() + $(window).scrollLeft()) {
+                left = width - panelWidth;
+            } else {
+                left = 0;
+            }
+            this.$panel.css({
+                position: 'absolute',
+                top: top,
+                left: left
+            });
+        },
+        _parse: function(string) {
+            if (string.indexOf('+') >= 0) {
+                return this.$element.next();
+            } else {
+                return $(this.options.panel);
+            }
+        },
+
+        // common  method
+        enable: function() {
+            this.enabled = true;
+        },
+        disable: function() {
+            this.enabled = false;
+        },
+        destory: function() {
+            this.hide();
+            this.$element.off('.dropdown');
+            this.$element.remove();
+            $(window).off('.dropdown');
+        }
+    };
+
+    Dropdown.defaults = {
+        namespace: 'dropdown',
+        skin: null,
+        panel: '+', //jquery selector to find content in the page, or '+' means adjacent siblings
+        clickoutHide: true, //When clicking outside of the dropdown, trigger hide event
+
+        //callback comes with corresponding event
+        onInit: null,
+        onShow: null,
+        onHide: null,
+        onChange: null
+    };
+
+    $.fn.dropdown = function(options) {
+        if (typeof options === 'string') {
+            var method = options;
+            var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : undefined;
+
+            return this.each(function() {
+                var api = $.data(this, 'dropdown');
+                if (typeof api[method] === 'function') {
+                    api[method].apply(api, method_arguments);
+                }
+            });
+        } else {
+            return this.each(function() {
+                if (!$.data(this, 'dropdown')) {
+                    $.data(this, 'dropdown', new Dropdown(this, options));
+                }
+            });
+        }
+    };
+}(jQuery));
